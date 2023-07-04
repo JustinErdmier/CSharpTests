bef54b172928830227ee5a46f3a9bfcd472eaa79
Justin Erdmier <48148443+JustinErdmier@users.noreply.github.com>
2023-07-04 12:38:41 -0400

    feat: add type for strings to source library

---
+++ Source/CSharpTests.Library/Strings.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpTests.Library;
+
+public sealed class Strings
+{
+    /// <summary>
+    ///     An auto-property of type <see langword="string" /> that is non-nullable and set with the
+    ///     <see langword="default" /> keyword with a the null-forgiving operator.
+    /// </summary>
+    public string NonNullableDefaultString { get; set; } = default!;
+
+    /// <summary>
+    ///     An auto-property of type <see langword="string" /> that is nullable and set with the
+    ///     <see langword="default" /> keyword.
+    /// </summary>
+    public string? NullableDefaultString { get; set; } = default;
+}
